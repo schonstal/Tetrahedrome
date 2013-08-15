16a00a40cfb378f1c4548ee78195e4a1ba7b592c
Josh Schonstal <josh@schonstal.org>
2013-08-15 00:57:12 -0700

    Sun

---
+++ Assets/Scripts/Tetrahedron.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tetrahedron : MonoBehaviour {
+  public Transform tetra;
+  public int triangleCount = 10;
+  public float triangleScale = 100f;
+  public float rotationSpeed = 1f;
+
+  void Start() {
+    for(int i = 1; i < triangleCount; i++) {
+      Transform clone = (Transform)Instantiate(tetra, Vector3.zero, Quaternion.identity);
+      clone.parent = transform;
+      clone.localRotation = Quaternion.identity;
+      clone.localPosition = new Vector3(0,0,0);
+      clone.localScale = new Vector3(triangleScale*i, triangleScale*i, triangleScale*i);
+    }
+  }
+	
+  void Update() {
+    transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed, Space.World);
+    transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
+  }
+}
