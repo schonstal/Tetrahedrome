97aff89ecf8e9618f9da4a5480fb571dc533ae8f
Josh Schonstal <josh@schonstal.org>
2013-08-15 21:01:03 -0700

    Quaternion Shmaternion

--- Assets/Scripts/CrystalSpawn.cs
+++ Assets/Scripts/CrystalSpawn.cs
@@ -2,14 +2,29 @@
 using System.Collections;
 
 public class CrystalSpawn : MonoBehaviour {
+  public Transform bigCrystal;
+  public Transform smallCrystal;
+  public Transform tinyCrystal;
 
-	// Use this for initialization
-	void Start () {
-	
+  public float scaleMin = 1f;
+  public float scaleMax = 2f;
+
+  public Vector3 rotationRate = new Vector3(0f,1f,0f);
+  public float floatRate = 1f; 
+  public float floatDistance = 0.5f;
+
+  private float sinAmt = 0f;
+
+	void Start() {
+    Transform main = (Transform)Instantiate(bigCrystal, Vector3.zero, Quaternion.identity);
+    main.parent = transform;
+
+    float scale = Random.Range(scaleMin,scaleMax);
+    main.localScale = new Vector3(scale, scale, scale);
+    main.localRotation = Quaternion.identity;
+    main.localPosition = new Vector3(0,0,0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update() {
 	}
 }
